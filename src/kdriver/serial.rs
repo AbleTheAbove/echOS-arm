35e872f3d64316b007e8b9acf29eac6e8a2b497a
able <abl3theabove@gmail.com>
2021-03-04 11:29:45 -0600

    kDriver split off

---
+++ src/kdriver/serial.rs
@@ -0,0 +1,17 @@
+use core::ptr;
+
+use crate::QEMU_UART0_VIRT;
+
+pub fn serial_log(str: &str) {
+    for byte in str.bytes() {
+        serialc(byte)
+    }
+    serialc(0x0A); // Adds a \n to the end of serial_log
+}
+
+fn serialc(byte: u8) {
+    let uart_register = QEMU_UART0_VIRT;
+    unsafe {
+        ptr::write_volatile(uart_register as *mut u8, byte);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,22 @@
 #![no_main]
 #![feature(global_asm)]
 
-use core::{panic::PanicInfo, ptr};
+use core::panic::PanicInfo;
 
 mod arch;
-use arch::aarch64::qemu_virt::QEMU_UART0_VIRT;
+pub use arch::aarch64::qemu_virt::QEMU_UART0_VIRT;
+mod kdriver;
+use kdriver::serial::serial_log;
 
 global_asm!(include_str!("start.s"));
 #[no_mangle]
 pub extern "C" fn boot() -> ! {
     serial_log("UART driver loaded");
+
     loop {}
 }
 
 #[panic_handler]
 fn on_panic(_info: &PanicInfo) -> ! {
     loop {}
-}
-
-fn serial_log(str: &str) {
-    for byte in str.bytes() {
-        serialc(byte)
-    }
-    serialc(0x0A); // Adds a \n to the end of serial_log
-}
-
-fn serialc(byte: u8) {
-    let uart_register = QEMU_UART0_VIRT;
-    unsafe {
-        ptr::write_volatile(uart_register as *mut u8, byte);
-    }
 }
